89cfce03c2be37a1885769e1d6a7cc535be5500c
easyxxu <107910342+easyxxu@users.noreply.github.com>
2023-06-07 11:03:52 +0900

    test: 프로필 설정 페이지 테스트

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import ProfileForm from "./components/Profile/ProfileForm";
+import ProfileSetting from "./pages/ProfileSetting";
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+const GlobalStyle = createGlobalStyle`
+${reset}
+input{
+  border:0;
+}
+button{
+  border: 0;
+  background-color: inherit;
+  cursor: pointer;
+}
+`;
 function App() {
   return (
     <div>
-      hello world
+      <GlobalStyle />
+      <ProfileSetting />
     </div>
   );
 }
